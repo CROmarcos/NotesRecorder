32245b74ab3d7c150369b4c01174d8d6aa17d460
Marko Pavlović <markopavlovic2010@hotmail.com>
2022-06-03 13:15:19 +0200

    Fixed syntax for refreshing state

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -2,7 +2,7 @@ import './Note.scss'
 import heart from '../../../src/images/heart.png'
 import trashCan from '../../../src/images/trashcan.png'
 import store from '../../state/store'
-import { markNote } from '../../state/actions'
+import { deleteNote, markNote } from '../../state/actions'
 
 const Note = props => {
     return (
@@ -12,8 +12,8 @@ const Note = props => {
                     <p>{props.title}</p>
                 </section>
                 <section className='card__title-important'>
-                    <button className='action' onClick={props.click}><img src={trashCan} alt="delete"></img></button>
-                    {props.important ? <button className='action' onClick={() => store.dispatch(markNote(props.noteId))} onSubmit={props.refresh}>Mark as important</button> : <button className='action' onClick={props.click}><img src={heart} alt="important note" /></button>}
+                    <button className='action' onClick={() => { store.dispatch(deleteNote(props.noteId)); props.refresh() }}><img src={trashCan} alt="delete"></img></button>
+                    {props.important ? <button className='action' onClick={() => { store.dispatch(markNote(props.noteId)); props.refresh() }}>Mark as important</button> : <button className='action' onClick={props.click}><img src={heart} alt="important note" /></button>}
                 </section>
             </div>
             <div className='card__body'>
--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -2,20 +2,18 @@ import { Link } from "react-router-dom"
 import Note from "../Note/Note"
 import store from "../../state/store"
 import { useState } from "react"
-import { deleteNote } from "../../state/actions"
 
 const NotesList = () => {
     const [list, setList] = useState(store.getState())
 
-    function removeNote(note) {
-        store.dispatch(deleteNote(note))
+    function refresh(){
         setList(store.getState())
     }
 
     return (
         <div>
             {list.map(note =>
-                <Note key={note.id} noteId={note.id} title={note.title} description={note.description} creationDate={note.creationDate} important={note.importance} click={() => removeNote(note.id)} />
+                <Note key={note.id} noteId={note.id} title={note.title} description={note.description} creationDate={note.creationDate} important={note.importance} refresh={refresh} />
             )}
             <Link to="/add"><button>Add new</button></Link>
         </div>
