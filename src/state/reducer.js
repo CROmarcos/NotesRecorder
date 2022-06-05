7b2001ed06e04d01d358ffe874bb7985b4ed6b58
Marko Pavlović <markopavlovic2010@hotmail.com>
2022-06-06 01:27:53 +0200

    Fixed note searching
    Payload is already ID.

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -39,7 +39,7 @@ const reducer = (state = initialState, action) => {
         case actions.DELETE:
             return state.filter(note => note.id !== action.payload)
         case actions.MARK:
-            return state.map(note => (note.id === action.payload.id) ? { ...note, importance: !this.importance } : note)
+            return state.map(note => (note.id === action.payload) ? { ...note, importance: !this.importance } : note)
         default:
             return state
     }
