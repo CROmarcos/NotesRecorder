bc8cbcf46b67f1aa03ea205dd2b13f276f39c66c
Marko Pavlović <markopavlovic2010@hotmail.com>
2022-06-10 14:59:04 +0200

    Added snapshot
    Snapshot for create new note

---
+++ src/__test__/CreateNote.test.js
@@ -0,0 +1,12 @@
+import { MemoryRouter } from "react-router-dom"
+import renderer from "react-test-renderer"
+import Note from "../components/Note/Note"
+
+it("note matches snapshot", () => {
+    const card = renderer.create(
+        <MemoryRouter>
+            <Note id="9999" title="Test note" description="Note made for testing" creationDate="today" lastModification="N/A" filtered={false} />
+        </MemoryRouter>
+    ).toJSON()
+    expect(card).toMatchSnapshot()
+})
